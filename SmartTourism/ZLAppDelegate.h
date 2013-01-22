8cca1566acfca6780276665648e9fe1fbe6ac8cf
Evan <wuzhiyong1989@hotmail.com>
2013-01-22 11:10:27 +0800

    init commit

---
+++ SmartTourism/ZLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZLAppDelegate.h
+//  SmartTourism
+//
+//  Created by Wu Zhiyong on 1/22/13.
+//  Copyright (c) 2013 Rikee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmartTourism/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmartTourism
+//
+//  Created by Wu Zhiyong on 1/22/13.
+//  Copyright (c) 2013 Rikee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZLAppDelegate class]));
+    }
+}
